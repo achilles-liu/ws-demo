e0355c73137f22fc28ac8875a223e7c9b4af1d45
achilles-liu <1027054310@qq.com>
2016-08-18 20:40:20 +0800

    tree

---
+++ ws-core/src/main/java/org/ws/core/structure/TTree.java
@@ -0,0 +1,222 @@
+package org.ws.core.structure;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.ws.core.structure.TTree.Node;
+
+/**
+ * tree
+ * @author Achilles Liu
+ *
+ * @param <T>
+ */
+public class TTree<T extends Node>{
+	
+	/**
+	 * the root of tree.
+	 */
+	private T root;
+	/**
+	 * the factory to create the node.
+	 */
+	private Factory<T> factory;
+	
+	/**
+	 * indicate whether the root node data is existing into the data collection.
+	 * true means the root data exists into the data collection.
+	 * if false without root node, the default method that create the root node will be called.
+	 * if false with root node, the root will be the specified root node.
+	 */
+	private boolean hasRootData = true;
+	
+	public TTree(List<Map<String,Object>> data,Factory<T> factory){
+		this(data,factory,true);
+	}
+	
+	public TTree(List<Map<String, Object>> data,Factory<T> factory,boolean hasRootData){
+		this(data,factory,hasRootData,null);
+	}
+	
+	public TTree(List<Map<String,Object>> data,Factory<T> factory,boolean hasRootData,T root){
+		this.factory = factory;
+		this.root = root;
+		this.hasRootData = hasRootData;
+		assemble(data);
+	}
+	
+	/**
+	 * the default method, which create the root.
+	 * if the data of root doesn't input,this method will be called.
+	 * @return
+	 */
+	private T createRoot(){
+		Map<String, Object> dt = new HashMap<String, Object>();
+		dt.put("id", 0);
+		dt.put("pid", -1);
+		return factory.create(dt);
+	}
+	
+	/**
+	 * assemble the tree using the inputed data.
+	 * @param data
+	 */
+	private void assemble(List<Map<String, Object>> data){
+		
+		if(this.hasRootData){
+			Map<String,Object> rd = findRoot(data);
+			if(rd == null) throw new IllegalArgumentException("illegal argument: [ hasRootData ]");
+			this.root = factory.create(rd);
+		}else{
+			if(this.root == null)this.root = createRoot();
+		}
+		
+		T tnode = null;
+		for(Map<String, Object> item:data){
+			tnode = factory.create(item);
+			int pid = tnode.getPid();
+			if(pid == -1 && root == null) {
+				root = tnode;
+				continue;
+			}
+			T parent = findParentByPid(root,pid);
+			if(parent != null){
+				parent.getChildren().add(tnode);
+			}
+		}
+	}
+
+	/**
+	 * check whether or not the data of root is existing in the inputed collections.
+	 * if existing, the data will be used to create a new root node.
+	 * @param data
+	 * @return
+	 */
+	private Map<String, Object> findRoot(List<Map<String,Object>> data){
+		Map<String, Object> rootData = null;
+		for(Map<String, Object> dt : data){
+			int pid = Integer.valueOf(String.valueOf(dt.get("pid")));
+			if(pid == -1){
+				rootData = dt;
+				break;
+			}
+		}
+		return rootData;
+	}
+	
+	/**
+	 * find the parent node according to the specified pid.
+	 * @param node
+	 * @param pid
+	 * @return
+	 */
+	private T findParentByPid(T node,int pid){
+		if(node.getId() == pid){
+			return node;
+		}else{
+			@SuppressWarnings("unchecked")
+			List<T> children = (List<T>) node.getChildren();
+			T parent = null;
+			for(T item : children){
+				parent = findParentByPid(item,pid);
+				if(parent != null) break;
+			}
+			return parent;
+		}
+	}
+	
+	public T getRoot(){
+		return this.root;
+	}
+	
+	/**
+	 * the factory to create the node.
+	 * the factory will be used to create this node,
+	 * Map<String, Object> datas = ...
+	 * TTree<UserNode> tree = new TTree<UserNode>(datas,new Factory<UserNode>(){
+	 *		public UserNode create(Map<String, Object> item) {
+	 *			UserNode userNode = new UserNode();
+	 *			userNode.setId(Integer.valueOf(String.valueOf(item.get("id"))));
+	 *			userNode.setPid(Integer.valueOf(String.valueOf(item.get("pid"))));
+	 *			userNode.setName(String.valueOf(item.get("name")));
+	 *			userNode.setAlias(String.valueOf(item.get("alias")));
+	 *			return userNode;
+	 *  }});
+	 * @author Achilles Liu
+	 *
+	 * @param <T>
+	 */
+	public interface Factory<T extends Node> {
+		T create(Map<String,Object> item);
+	}
+	
+	/**
+	 * the top class of node.
+	 * it can be implemented to customize the specified node.
+	 * for example, the "UserNode" node can be customized as follows,
+	 * public class UserNode extends Node{
+	 *	private String name;
+	 *	private String alias;
+	 *  ...
+	 *	public String getName() {
+	 *		return name;
+	 *	}
+	 *	public void setName(String name) {
+	 *		this.name = name;
+	 *	}
+	 *	public String getAlias() {
+	 *		return alias;
+	 *	}
+	 *	public void setAlias(String alias) {
+	 *		this.alias = alias;
+	 *	}
+	 *  ...
+	 * }
+	 * @author Achilles Liu
+	 *
+	 */
+	public static class Node{
+		/**
+		 * id
+		 */
+		private int id;
+		/**
+		 * parent id
+		 */
+		private int pid;
+		/**
+		 * children of the node
+		 */
+		private List<Node> children = new ArrayList<Node>();
+		
+		public Node(){}
+		
+		public Node(int id, int pid){
+			this.id = id;
+			this.pid = pid;
+		}
+		
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		
+		public int getPid() {
+			return pid;
+		}
+		public void setPid(int pid) {
+			this.pid = pid;
+		}
+		public List<Node> getChildren() {
+			return children;
+		}
+		
+		public void setChildren(List<Node> children) {
+			this.children = children;
+		}
+	}
+}
---
+++ ws-core/src/main/java/org/ws/core/structure/Test.java
@@ -0,0 +1,128 @@
+package org.ws.core.structure;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.ws.core.structure.TTree.Factory;
+import org.ws.core.structure.TTree.Node;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * test tree
+ * @author Achilles Liu
+ *
+ */
+public class Test {
+	
+	static List<Map<String,Object>> datas = new ArrayList<Map<String,Object>>();
+	static List<Map<String,Object>> datas2 = new ArrayList<Map<String,Object>>();
+	static{
+		Map<String,Object> m0 = new HashMap<String,Object>();
+		m0.put("id", 0);
+		m0.put("pid", -1);
+		m0.put("name", "root");
+		m0.put("alias", "root");
+		Map<String,Object> m1 = new HashMap<String,Object>();
+		m1.put("id", 1);
+		m1.put("pid", 0);
+		m1.put("name", "node1");
+		m1.put("alias", "alias1");
+		Map<String,Object> m2 = new HashMap<String,Object>();
+		m2.put("id", 2);
+		m2.put("pid", 0);
+		m2.put("name", "node2");
+		m2.put("alias", "alias2");
+		Map<String,Object> m3 = new HashMap<String,Object>();
+		m3.put("id", 3);
+		m3.put("pid", 1);
+		m3.put("name", "node11");
+		m3.put("alias", "alias11");
+		Map<String,Object> m4 = new HashMap<String,Object>();
+		m4.put("id", 4);
+		m4.put("pid", 1);
+		m4.put("name", "node12");
+		m4.put("alias", "alias12");
+		Map<String,Object> m5 = new HashMap<String,Object>();
+		m5.put("id", 5);
+		m5.put("pid", 2);
+		m5.put("name", "node21");
+		m5.put("alias", "alias21");
+		Map<String,Object> m6 = new HashMap<String,Object>();
+		m6.put("id", 6);
+		m6.put("pid", 2);
+		m6.put("name", "node22");
+		m6.put("alias", "alias22");
+		datas.add(m4);
+		datas.add(m2);
+		datas.add(m3);
+		datas.add(m6);
+		datas.add(m5);
+		datas.add(m1);
+		datas.add(m0);
+		
+		datas2.add(m4);
+		datas2.add(m2);
+		datas2.add(m3);
+		datas2.add(m6);
+		datas2.add(m5);
+		datas2.add(m1);
+		//datas2.add(m0);
+	}
+	
+	public static class UserNode extends Node{
+		private String name;
+		private String alias;
+		
+		public UserNode(){}
+		
+		public UserNode(int id,int pid,String name,String alias){
+			super(id,pid);
+			this.name = name;
+			this.alias = alias;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getAlias() {
+			return alias;
+		}
+		public void setAlias(String alias) {
+			this.alias = alias;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		TTree<UserNode> tree = new TTree<UserNode>(datas,new Factory<UserNode>(){
+			public UserNode create(Map<String, Object> item) {
+				UserNode un = new UserNode();
+				un.setId(Integer.valueOf(String.valueOf(item.get("id"))));
+				un.setPid(Integer.valueOf(String.valueOf(item.get("pid"))));
+				un.setName(String.valueOf(item.get("name")));
+				un.setAlias(String.valueOf(item.get("alias")));
+				return un;
+			}});
+		TTree<UserNode> tree2 = new TTree<UserNode>(datas2,new Factory<UserNode>(){
+			public UserNode create(Map<String, Object> item) {
+				UserNode un = new UserNode();
+				un.setId(Integer.valueOf(String.valueOf(item.get("id"))));
+				un.setPid(Integer.valueOf(String.valueOf(item.get("pid"))));
+				un.setName(String.valueOf(item.get("name")));
+				un.setAlias(String.valueOf(item.get("alias")));
+				return un;
+			}},false,new UserNode(0,-1,"root2","root2"));
+		ObjectMapper mapper = new ObjectMapper();
+		System.out.println(mapper.writeValueAsString(tree));
+		System.out.println(mapper.writeValueAsString(tree.getRoot()));
+		
+		System.out.println(mapper.writeValueAsString(tree2));
+		System.out.println(mapper.writeValueAsString(tree2.getRoot()));
+	}
+
+}
